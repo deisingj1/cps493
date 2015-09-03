ecc8fcffe100b8557957be52a0d9480f9895bd6f
deisingj1 <deisingj1@hawkmail.newpaltz.edu>
2015-09-03 14:11:35 +0000

    Added table and add/del buttons

--- index.php
+++ index.php
@@ -20,6 +20,40 @@
   <body>
       <div class="container">
     <h1>Hello, world!</h1>
+    
+    <button type="button" class="btn btn-danger"><i class="glyphicon glyphicon-plus"></i> New Record </button>
+    <a href="#" class="btn btn-success"><i class="glyphicon glyphicon-trash"></i> Delete </a>
+    
+    <table class="table table-striped">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Username</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <th scope="row">3</th>
+            <td>Larry</td>
+            <td>the Bird</td>
+            <td>@twitter</td>
+          </tr>
+        </tbody>
+      </table>
 
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
