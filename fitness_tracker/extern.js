9a8e6c554c69109cd7e7b9ef6f961db4512e021b
deisingj1 <deisingj1@hawkmail.newpaltz.edu>
2015-09-20 12:17:08 -0400

    Laid out "add" window for meals/workouts

--- fitness_tracker/extern.js
+++ fitness_tracker/extern.js
@@ -5,4 +5,11 @@ $(document).ready(function() {
 		$(".menubar").toggleClass("col-sm-3 col-sm-1");
 		$(".contentPane").toggleClass("col-sm-9 col-sm-11");
 	});
+	//add dropdown code
+	//do initial hide
+	$("#addDropdown").hide(0);
+	//event handler for add button
+	$("#addButton").on('click', function() {
+		$("#addDropdown").slideDown(200);
+	});
 }); 
