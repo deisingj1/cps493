a27fd49f8ec14d323791287f3e84c267c917e883
Jesse Deisinger <deisingj1@hawkmail.newpaltz.edu>
2015-11-11 21:15:01 -0500

    Added labels for edit page

--- fitness_tracker/php/Views/meals/edit.php
+++ fitness_tracker/php/Views/meals/edit.php
@@ -1,7 +1,22 @@
 <form class="form-horizontal" action="?action=save" method="post">
-	<input type="text" class="form-control" name="meal" value="<?=$model['meal']?>">
-	<input type="text" class="form-control" name="time" value="<?=$model['time']?>">
-	<input type="text" class="form-control" name="calories" value="<?=$model['calories']?>">
+	<div class="form-group">
+		<label for="inputMeal" class="col-sm-2 control-label">Meal</label>
+		<div class="col-sm-10">
+			<input type="text" class="form-control" id="inputMeal" name="meal" value="<?=$model['meal']?>">
+		</div>
+	</div>	
+	<div class="form-group">
+		<label for="inputTime" class="col-sm-2 control-label">Time</label>
+		<div class="col-sm-10">
+			<input type="text" id="inputTime" class="form-control" name="time" value="<?=$model['time']?>">
+		</div>
+	</div> 	
+	<div class="form-group">
+		<label for="inputCalories" class="col-sm-2 control-label">Calories</label>
+		<div class="col-sm-10">
+			<input type="text" id="inputCalories" class="form-control" name="calories" value="<?=$model['calories']?>">
+		</div>
+	</div>	
 	<input type="hidden" name="id" value="<?=$model['id']?>">
 	<input type="submit" class="btn btn-default form-control" value="Submit">
 </form>
