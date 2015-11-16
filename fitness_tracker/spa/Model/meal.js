a6cde97c5d5c1749ff5d740d302c86722f34a632
deisingj1 <deisingj1@hawkmail.newpaltz.edu>
2015-11-16 19:55:04 +0000

    node models complete

---
+++ fitness_tracker/spa/Model/meal.js
@@ -0,0 +1,62 @@
+var mysql = require("mysql");
+
+module.exports =  {
+    blank: function(){ return {} },
+    get: function(id, ret){
+        var conn = GetConnection();
+        var sql = 'SELECT * FROM FT_meals ';
+        if(id){
+          sql += " WHERE id = " + id;
+        }
+        conn.query(sql, function(err,rows){
+          ret(err,rows);
+          conn.end();
+        });        
+    },
+    delete: function(id, ret){
+        var conn = GetConnection();
+        conn.query("DELETE FROM FT_meals WHERE id = " + id, function(err,rows){
+          ret(err);
+          conn.end();
+        });        
+    },
+    save: function(row, ret){
+        var sql;
+        var conn = GetConnection();
+        //  TODO Sanitize
+        if (row.id) {
+				  sql = " Update FT_meals "
+							+ " Set meal=?, time=?, calories=? "
+						  + " WHERE id = ? ";
+			  }else{
+				  sql = "INSERT INTO FT_meals "
+						  + " (meal, time, calories, create_time) "
+						  + "VALUES (?, ?, ?, Now() ) ";				
+			  }
+
+        conn.query(sql, [row.workout, row.time, row.calories, row.id],function(err,data){
+          if(!err && !row.id){
+            row.id = data.insertId;
+          }
+          ret(err, row);
+          conn.end();
+        });        
+    },
+    validate: function(row){
+      var errors = {};
+      
+      if(!row.Name) errors.Name = "is required"; 
+      
+      return errors.length ? errors : false;
+    }
+};  
+
+function GetConnection(){
+        var conn = mysql.createConnection({
+          host: "localhost",
+          user: "jesse",
+          password: "9221",
+          database: "c9"
+        });
+    return conn;
+}
---
+++ fitness_tracker/spa/express.js
@@ -0,0 +1,80 @@
+var express = require('express'),
+    app = express();
+var bodyParser = require('body-parser');
+var meal = require("./Model/meal");
+var workout = require("./Model/workout")
+
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get("/meal", function(req, res){
+  
+  meal.get(null, function(err, rows){
+    res.send(rows);
+  })
+    
+})
+.get("/meal/:id", function(req, res){
+  
+  meal.get(req.params.id, function(err, rows){
+    res.send(rows[0]);
+  })
+  
+})
+.post("/meal", function(req, res){
+  var errors = meal.validate(req.body);
+  if(errors){
+    res.status(500).send(errors);
+    return;
+  }
+  meal.save(req.body, function(err, row){
+    res.send(row);
+  })
+})
+.delete("/meal/:id", function(req, res){
+  
+  meal.delete(req.params.id, function(err, rows){
+      if(err){
+        res.status(500).send(err);
+      }else{
+        res.send(req.params.id);
+      }
+  })
+  
+})
+app.get("/workout", function(req, res){
+  
+  workout.get(null, function(err, rows){
+    res.send(rows);
+  })
+    
+})
+.get("/workout/:id", function(req, res){
+  
+  workout.get(req.params.id, function(err, rows){
+    res.send(rows[0]);
+  })
+  
+})
+.post("/workout", function(req, res){
+  var errors = workout.validate(req.body);
+  if(errors){
+    res.status(500).send(errors);
+    return;
+  }
+  workout.save(req.body, function(err, row){
+    res.send(row);
+  })
+})
+.delete("/workout/:id", function(req, res){
+  
+  workout.delete(req.params.id, function(err, rows){
+      if(err){
+        res.status(500).send(err);
+      }else{
+        res.send(req.params.id);
+      }
+  })
+  
+})
+app.listen(process.env.PORT);
